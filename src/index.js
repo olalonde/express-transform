7c53ec48481f6d32f54e1aa6eee0967c1152a28f
Olivier Lalonde <olalonde@gmail.com>
2015-11-24 15:19:39 +0800

    fix(events): Correctly forward stream events

--- src/index.js
+++ src/index.js
@@ -2,60 +2,102 @@ import stream from 'stream';
 import initDebug from 'debug';
 const debug = initDebug('express-transform');
 
-class ResProxy extends stream.Writable {
-  constructor(res) {
-    super();
-    this.res = res;
-    this.origWrite = res.write;
-    this.origEnd = res.end;
-  }
+const spec = {
+  readable: {
+    events: [
+      'close',
+      'data',
+      'end',
+      'error',
+      'readable',
+    ],
+    methods: [
+      'isPaused',
+      'pause',
+      'pipe',
+      'read',
+      'resume',
+      'setEncoding',
+      'unpipe',
+      'unshift',
+      'wrap',
+    ],
+  },
+  writable: {
+    events: [
+      'drain',
+      'error',
+      'finish',
+      'pipe',
+      'unpipe',
+    ],
+    methods: [
+      'cork',
+      'end',
+      'setDefaultEncoding',
+      'uncork',
+      'write',
+    ],
+  },
+};
 
-  write(...args) {
-    // console.log('res.write');
-    return this.origWrite.apply(this.res, args);
-  }
-
-  end(...args) {
-    // console.log('res.end');
-    return this.origEnd.apply(this.res, args);
-  }
-}
+spec.stream = {
+  events: spec.readable.events.concat(spec.writable.events),
+  methods: spec.readable.methods.concat(spec.writable.methods),
+};
 
 const patch = (res) => {
-  const resProxy = new ResProxy(res);
+  debug('patching res');
 
-  res.transforms = [ new stream.PassThrough() ];
-  const first = () => {
-    return res.transforms[0];
+  const _end = res.end.bind(res);
+  const _write = res.write.bind(res);
+  const _emit = res.emit.bind(res);
+
+  let lastStream = res;
+
+  const resProxy = new stream.Writable();
+  resProxy.write = _write;
+  resProxy.end = _end;
+
+  // Redirect events:
+  // 1) pass-xyz => res.emit(xyz)
+  // 2) writable events => resProxy.emit(event)
+  // 3) other events => res.emit(event)
+  res.emit = (evName, ...args) => {
+    const matches = evName.match(/^pass\-(.*)/);
+    const isWritableEvent = spec.writable.events.indexOf(evName) !== -1;
+
+    if (matches) {
+      const name = matches[1];
+      debug(`emitting ${name} to res`);
+      return _emit(name, ...args);
+    } else if (isWritableEvent) {
+      debug(`emitting ${evName} to resProxy`);
+      return resProxy.emit(evName, ...args);
+    }
+
+    return _emit(evName, ...args);
   };
-  const last = () => {
-    return res.transforms[res.transforms.length - 1];
-  };
+
+  const pass = new stream.PassThrough();
+
+  // Forward res stream methods to pass
+  spec.stream.methods.forEach((methodName) => {
+    res[methodName] = pass[methodName].bind(pass);
+  });
+
+  // Forward pass stream events to res
+  spec.stream.events.forEach((evName) => {
+    pass.on(evName, (...args) => res.emit('pass-' + evName, ...args));
+  });
 
   res.transform = (s) => {
-    last().pipe(s);
-    res.transforms.push(s);
+    lastStream.unpipe(resProxy);
+    lastStream.pipe(s).pipe(resProxy);
+    lastStream = s;
   };
 
-  let streamConnected = false;
-  const connectStream = () => {
-    if (streamConnected) return;
-    debug('connecting transform stream to response');
-    streamConnected = true;
-    last().pipe(resProxy);
-  };
-  // todo: only call res.write/res.end if res.transform
-  // is called at least once
-  res.write = (...args) => {
-    connectStream();
-    // console.log('passthrough res.write');
-    return first().write(...args);
-  };
-  res.end = (...args) => {
-    connectStream();
-    // console.log('passthrough res.end');
-    return first().end(...args);
-  };
+  res.pipe(resProxy);
 };
 
 
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+/* eslint no-unused-vars:0 */
 import test from 'blue-tape';
 import http from 'http';
 import express from 'express';
@@ -5,6 +6,14 @@ import fetch from 'node-fetch';
 import transform from '../src';
 import zlib from 'zlib';
 import onHeaders from 'on-headers';
+import fs from 'fs';
+import path from 'path';
+import initFsBlobStore from 'fs-blob-store';
+
+const fsblobstore = initFsBlobStore(path.join(__dirname, 'data'));
+
+const logoPath = path.join(__dirname, 'data/blockai-logo.png');
+const logoContent = fs.readFileSync(logoPath);
 
 let httpServer;
 
@@ -12,12 +21,14 @@ const setup = () => {
   const app = express();
   app.use(transform());
   app.use((req, res, next) => {
-    const gzip = zlib.createGzip();
-    res.transform(gzip);
-    onHeaders(res, function gzipOnHeaders() {
-      this.setHeader('Content-Encoding', 'gzip');
-      this.removeHeader('Content-Length');
-    });
+    if ('gzip' in req.query) {
+      const gzip = zlib.createGzip();
+      res.transform(gzip);
+      onHeaders(res, function gzipOnHeaders() {
+        this.setHeader('Content-Encoding', 'gzip');
+        this.removeHeader('Content-Length');
+      });
+    }
     next();
   });
 
@@ -29,7 +40,24 @@ const setup = () => {
     res.json({ hello: 'world!' });
   });
 
-  httpServer = http.createServer(app).listen();
+  app.use('/readstream', (req, res) => {
+    const readStream = fs.createReadStream(logoPath);
+    res.status(200);
+    res.set('Content-Type', 'image/png');
+    res.set('Cache-Control', 'max-age=31536000, public');
+    readStream.pipe(res);
+  });
+
+  app.use('/fsblobstore', (req, res, next) => {
+    const readStream = fsblobstore.createReadStream('blockai-logo.png');
+    readStream.on('error', next);
+    res.status(200);
+    res.set('Content-Type', 'image/png');
+    res.on('finish', () => {});
+    readStream.pipe(res);
+  });
+
+  httpServer = http.createServer(app).listen(process.env.PORT);
   const httpPort = httpServer.address().port;
   const url = `http://localhost:${httpPort}`;
   return Promise.resolve({ url });
@@ -48,7 +76,25 @@ test('start server', (t) => {
   });
 });
 
-test('res.end', (t) => {
+test('/hello?gzip', (t) => {
+  return fetch(`${url}/hello`)
+    .then((res) => res.text())
+    .then((body) => {
+      t.equal(body, 'Hello World!');
+    });
+});
+
+test('res.json?gzip', (t) => {
+  return fetch(`${url}/json`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((body) => {
+      t.deepEqual(body, { hello: 'world!' });
+    });
+});
+
+test('/hello', (t) => {
   return fetch(`${url}/hello`)
     .then((res) => res.text())
     .then((body) => {
@@ -63,6 +109,38 @@ test('res.json', (t) => {
     })
     .then((body) => {
       t.deepEqual(body, { hello: 'world!' });
+    });
+});
+
+test('readstream', (t) => {
+  return fetch(`${url}/readstream`)
+    .then((res) => {
+      const body = res.body;
+      return new Promise((resolve, reject) => {
+        const buffers = [];
+        body.on('data', (chunk) => buffers.push(chunk));
+        body.on('end', () => resolve(Buffer.concat(buffers)));
+        body.on('error', (err) => reject(err));
+      });
+    })
+    .then((buf) => {
+      t.deepEqual(buf, logoContent);
+    });
+});
+
+test('fsblobstore', (t) => {
+  return fetch(`${url}/fsblobstore`)
+    .then((res) => {
+      const body = res.body;
+      return new Promise((resolve, reject) => {
+        const buffers = [];
+        body.on('data', (chunk) => buffers.push(chunk));
+        body.on('end', () => resolve(Buffer.concat(buffers)));
+        body.on('error', (err) => reject(err));
+      });
+    })
+    .then((buf) => {
+      t.deepEqual(buf, logoContent);
     });
 });
 
