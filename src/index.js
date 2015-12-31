533bca66ca6ac1565f65d7a7263447bc25bc59b3
Philip Salzmann <philip.salzmann@red-six.net>
2015-12-31 18:46:08 +0100

    fix: make sure the HTTP zero chunk is actually sent

--- src/index.js
+++ src/index.js
@@ -41,11 +41,6 @@ const spec = {
   },
 };
 
-spec.stream = {
-  events: spec.readable.events.concat(spec.writable.events),
-  methods: spec.readable.methods.concat(spec.writable.methods),
-};
-
 const patch = (res) => {
   debug('patching res');
 
@@ -53,16 +48,23 @@ const patch = (res) => {
   const _write = res.write.bind(res);
   const _emit = res.emit.bind(res);
 
-  let lastStream = res;
-
   const resProxy = new stream.Writable();
   resProxy.write = _write;
   resProxy.end = _end;
 
-  // Redirect events:
-  // 1) pass-xyz => res.emit(xyz)
-  // 2) writable events => resProxy.emit(event)
-  // 3) other events => res.emit(event)
+  /*
+   * Events emitted by the PassThrough stream (i.e. "pass-*") will be
+   * re-emitted by the ServerResponse. However, the "pass-finish" event
+   * is not re-emitted, as this causes the ServerResponse to remove its
+   * connection socket (see NodeJS _http_socket.js), which in turn prevents it from
+   * sending the zero chunk (within ServerResponse.end). The ServerResponse
+   * does however emit its own "finish" event later on.
+   *
+   * Writable events will be emitted by resProxy.
+   *
+   * All other events (e.g. custom), will be emitted by the ServerResponse,
+   * as expected.
+   */
   res.emit = (evName, ...args) => {
     const matches = evName.match(/^pass\-(.*)/);
     const isWritableEvent = spec.writable.events.indexOf(evName) !== -1;
@@ -70,6 +72,10 @@ const patch = (res) => {
     if (matches) {
       const name = matches[1];
       debug(`emitting ${name} to res`);
+
+      if (name === 'finish') {
+        return false;
+      }
       return _emit(name, ...args);
     } else if (isWritableEvent) {
       debug(`emitting ${evName} to resProxy`);
@@ -82,22 +88,23 @@ const patch = (res) => {
   const pass = new stream.PassThrough();
 
   // Forward res stream methods to pass
-  spec.stream.methods.forEach((methodName) => {
+  spec.writable.methods.forEach((methodName) => {
     res[methodName] = pass[methodName].bind(pass);
   });
 
   // Forward pass stream events to res
-  spec.stream.events.forEach((evName) => {
+  spec.writable.events.forEach((evName) => {
     pass.on(evName, (...args) => res.emit('pass-' + evName, ...args));
   });
+
+  let lastStream = pass;
+  lastStream.pipe(resProxy);
 
   res.transform = (s) => {
     lastStream.unpipe(resProxy);
     lastStream.pipe(s).pipe(resProxy);
     lastStream = s;
   };
-
-  res.pipe(resProxy);
 };
 
 
